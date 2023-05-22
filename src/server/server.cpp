a331c66c5aa70ab15d133cc8bdf3f51139251fd9
kakushawn <kakushawn@gmail.com>
2023-05-22 09:02:29 +0800

    dos2unix

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,25 +1,51 @@
 #include <arpa/inet.h>
 #include <errno.h>
+#include <pthread.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 #include <cstring>
 #include <iostream>
+#include <thread>
 
-#define BACKLOG 2
+#define BACKLOG 20
+#define BUFF_SIZE 1024
+
+void run(int id, const int &sockfd) {
+    std::cout << "[" << id << "]: accepting...\n";
+    struct sockaddr_storage their_addr;
+    socklen_t talen = sizeof(their_addr);
+    int sockfd_client = accept(sockfd, (struct sockaddr *)&their_addr, &talen);
+
+    /* receive */
+    std::cout << "[" << id << "]: receiving...\n";
+    char buff[BUFF_SIZE] = {0};
+    recv(sockfd_client, buff, BUFF_SIZE, 0);
+    std::cout << "[" << id << "]: message from client: " << buff << std::endl;
+
+    /* send */
+    std::string response(buff);
+
+    if (send(sockfd_client, response.c_str(), response.size(), 0) < 0) {
+        std::cout << "[" << id << "]: Could not send. errno: " << errno << std::endl;
+    } else {
+        std::cout << "[" << id << "]: Finished\n\n";
+    }
+    close(sockfd_client);
+}
 
 int main() {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
-        std::cerr << "Cannot create socket.\n";
+        std::cout << "Cannot create socket.\n";
         exit(1);
     }
 
     int opt = 1;
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
                    sizeof(opt)) < 0) {
-        std::cerr << "Cannot setsockopt.\n";
+        std::cout << "Cannot setsockopt.\n";
         return 1;
     }
 
@@ -27,45 +53,28 @@ int main() {
         .sin_family = AF_INET, .sin_port = htons(9002), .sin_addr = INADDR_ANY};
 
     if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        std::cerr << "Cannot bind.\n";
-        std::cerr << "errno: " << errno << std::endl;
+        std::cout << "Cannot bind.\n";
+        std::cout << "errno: " << errno << std::endl;
         return 1;
     }
 
-    std::cerr << "listening:\n";
-    if (listen(sockfd, BACKLOG) == -1) {
-        std::cerr << "I am deaf.\n";
+    std::cout << "listening:\n";
+    if (listen(sockfd, 1) == -1) {
+        std::cout << "I am deaf.\n";
         return 1;
     }
 
     int counter = 0;
-    struct sockaddr_storage their_addr;
-    socklen_t talen = sizeof(their_addr);
     while (true) {
-        std::cout << "[" << counter << "]:\n";
-        std::cerr << "accepting...\n";
-        int sockfd_client = accept(sockfd, (struct sockaddr *)&their_addr, &talen);
-
-        /* receive */
-        std::cerr << "receiving...\n";
-        char buff[1024] = {0};
-        recv(sockfd_client, buff, 1024, 0);
-        std::cerr << "message from client: " << buff << std::endl;
-
-        /* send */
-        // std::string response = "Bye!";
-        std::string response(buff);
-
-        if (send(sockfd_client, response.c_str(), response.size(), 0) < 0) {
-            std::cerr << "Could not send. errno: " << errno << std::endl;
-        } else {
-            std::cerr << "[" << counter << "]" << "Finished\n\n";
-        }
-        ++counter;
-        close(sockfd_client);
+        std::thread worker(run, counter++, sockfd);
+        // std::thread worker2(run, counter++, sockfd);
+        // std::thread worker3(run, counter++, sockfd);
+        worker.join();
+        // worker2.join();
+        // worker3.join();
     }
 
-    std::cerr << "Shut down.\n";
+    std::cout << "Shut down.\n";
     shutdown(sockfd, SHUT_RDWR);
 
     return 0;
