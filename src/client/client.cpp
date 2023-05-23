641eaffb46fbea66adecc633f2aeca38c94a6f0c
kakushawn <kakushawn@gmail.com>
2023-05-23 13:36:38 +0800

    tab size to 4

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -11,54 +11,53 @@
 
 int main(int argc, char **argv)
 {
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) {
-		std::cout << "Cannot create socket.\n";
-		exit(1);
-	}
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        std::cout << "Cannot create socket.\n";
+        exit(1);
+    }
 
-	struct sockaddr_in addr = {
-	    .sin_family = AF_INET,
-	    .sin_port = htons(9002),
-	    .sin_addr = INADDR_ANY};
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(9002),
+        .sin_addr = INADDR_ANY};
 
-	if (connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-		std::cout << "Cannot connect.\n";
-		std::cout << "errno: " << errno << std::endl;
-		exit(1);
-	}
-	std::cout << "Connected.\n";
+    if (connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        std::cout << "Cannot connect.\n";
+        std::cout << "errno: " << errno << std::endl;
+        exit(1);
+    }
+    std::cout << "Connected.\n";
 
-	/* send */
-	char msg[LINE_LENGTH_LIMIT] = {0};
-	if (argc == 1) {
-		std::cin.getline(msg, LINE_LENGTH_LIMIT);
-	}
-	else {
-		unsigned int c = 0;
-		while (c < 1023 && argv[1][c] != '\0') {
-			msg[c] = argv[1][c];
-			++c;
-		}
-		msg[c] = '\0';
-	}
-	if (send(sockfd, msg, strlen(msg), 0) < 0) {
-		std::cout << "Cannot send message.\n";
-		std::cout << "errno: " << errno << std::endl;
-		exit(1);
-	}
+    /* send */
+    char msg[LINE_LENGTH_LIMIT] = {0};
+    if (argc == 1) {
+        std::cin.getline(msg, LINE_LENGTH_LIMIT);
+    } else {
+        unsigned int c = 0;
+        while (c < 1023 && argv[1][c] != '\0') {
+            msg[c] = argv[1][c];
+            ++c;
+        }
+        msg[c] = '\0';
+    }
+    if (send(sockfd, msg, strlen(msg), 0) < 0) {
+        std::cout << "Cannot send message.\n";
+        std::cout << "errno: " << errno << std::endl;
+        exit(1);
+    }
 
-	/* receive */
-	char buff[1024] = {0};
-	if (recv(sockfd, &buff, 1024, 0) < 0) {
-		std::cout << "Cannot receive message.\n";
-		std::cout << "errno: " << errno << std::endl;
-		exit(1);
-	}
-	std::cout << "Message from server: " << buff << std::endl;
+    /* receive */
+    char buff[1024] = {0};
+    if (recv(sockfd, &buff, 1024, 0) < 0) {
+        std::cout << "Cannot receive message.\n";
+        std::cout << "errno: " << errno << std::endl;
+        exit(1);
+    }
+    std::cout << "Message from server: " << buff << std::endl;
 
-	std::cout << "Shut down client.\n";
-	close(sockfd);
+    std::cout << "Shut down client.\n";
+    close(sockfd);
 
-	return 0;
+    return 0;
 }
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -12,73 +12,66 @@
 #define BACKLOG 10
 #define BUFF_SIZE 1024
 
-void SignalHandler(int signum)
-{
-        std::cout << "Receied signal: " << signum << std::endl;
-        exit(signum);
-}
-
 int main()
 {
-        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if (sockfd < 0) {
-                std::cout << "Cannot create socket.\n";
-        	exit(1);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        std::cout << "Cannot create socket.\n";
+        exit(1);
+    }
+
+    int opt = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
+                   sizeof(opt)) < 0) {
+        std::cout << "Cannot setsockopt.\n";
+        return 1;
+    }
+
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET, .sin_port = htons(9002), .sin_addr = INADDR_ANY};
+
+    if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        std::cout << "Cannot bind.\n";
+        std::cout << "errno: " << errno << std::endl;
+        return 1;
+    }
+
+    std::cout << "listening:\n";
+    if (listen(sockfd, 1) == -1) {
+        std::cout << "I am deaf.\n";
+        return 1;
+    }
+
+    int id = 0;
+    struct sockaddr_storage client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    int sockfd_client;
+    while (1) {
+        std::cout << "[" << id << "]: accepting...\n";
+        sockfd_client = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
+
+        /* receive */
+        std::cout << "[" << id << "]: receiving...\n";
+        char buff[BUFF_SIZE] = {0};
+        if (recv(sockfd_client, buff, BUFF_SIZE, 0) < 0) {
+            std::cout << "Cannot receive message. errno: " << errno << std::endl;
+        } else {
+            std::cout << "[" << id << "]: message from client: " << buff << std::endl;
+            /* send */
+            std::string response(buff);
+            if (send(sockfd_client, response.c_str(), response.size(), 0) < 0) {
+                std::cout << "[" << id << "]: Could not send message. errno: " << errno << std::endl;
+            } else {
+                std::cout << "[" << id << "]: Finished\n\n";
+            }
         }
 
-        int opt = 1;
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
-                       sizeof(opt)) < 0) {
-                std::cout << "Cannot setsockopt.\n";
-                return 1;
-        }
+        ++id;
+    	close(sockfd_client);
+    }
 
-        struct sockaddr_in addr = {
-            .sin_family = AF_INET, .sin_port = htons(9002), .sin_addr = INADDR_ANY};
+    close(sockfd);
+    std::cout << "Shut down server.\n";
 
-        if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-                std::cout << "Cannot bind.\n";
-                std::cout << "errno: " << errno << std::endl;
-                return 1;
-        }
-
-        std::cout << "listening:\n";
-        if (listen(sockfd, 1) == -1) {
-                std::cout << "I am deaf.\n";
-                return 1;
-        }
-
-        std::signal(SIGINT, SignalHandler);
-        std::signal(SIGTERM, SignalHandler);
-
-        int id = 0;
-	struct sockaddr_storage client_addr;
-	socklen_t client_addr_len = sizeof(client_addr);
-        while(1) {
-		std::cout << "[" << id << "]: accepting...\n";
-		int sockfd_client = accept(sockfd, (struct sockaddr *) &client_addr, &client_addr_len);
-
-		/* receive */
-		std::cout << "[" << id << "]: receiving...\n";
-		char buff[BUFF_SIZE] = {0};
-		recv(sockfd_client, buff, BUFF_SIZE, 0);
-		std::cout << "[" << id << "]: message from client: " << buff << std::endl;
-
-		/* send */
-		std::string response(buff);
-		if (send(sockfd_client, response.c_str(), response.size(), 0) < 0) {
-			std::cout << "[" << id << "]: Could not send message. errno: " << errno << std::endl;
-		}
-		else {
-			std::cout << "[" << id << "]: Finished\n\n";
-		}
-
-		close(sockfd_client);
-		++id;
-	}
-
-        std::cout << "Shut down server.\n";
-        close(sockfd);
-
-        return 0;
+    return 0;
 }
