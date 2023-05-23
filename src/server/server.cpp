00d7e6a9b2c4af99d930da0b272f1b29cfa82687
kakushawn <kakushawn@gmail.com>
2023-05-23 11:14:48 +0800

    remove multi-threading; replace spaces to tab

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,6 +1,5 @@
 #include <arpa/inet.h>
 #include <errno.h>
-#include <pthread.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -8,49 +7,12 @@
 #include <csignal>
 #include <cstring>
 #include <iostream>
-#include <thread>
 #include <vector>
 
 #define BACKLOG 10
 #define BUFF_SIZE 1024
-#define WORKER_NUM 4;
 
-struct arg_struct {
-        int id;
-        int sockfd;
-};
-
-void *run(void *args)
-{
-	struct arg_struct *arguments = (struct arg_struct *)args;
-        int id = arguments->id;
-        int sockfd = arguments->sockfd;
-        std::cout << "[" << id << "]: accepting...\n";
-        struct sockaddr_storage their_addr;
-        socklen_t talen = sizeof(their_addr);
-        while (1) {
-                int sockfd_client = accept(sockfd, (struct sockaddr *)&their_addr, &talen);
-
-                /* receive */
-                std::cout << "[" << id << "]: receiving...\n";
-                char buff[BUFF_SIZE] = {0};
-                recv(sockfd_client, buff, BUFF_SIZE, 0);
-                std::cout << "[" << id << "]: message from client: " << buff << std::endl;
-
-                /* send */
-                std::string response(buff);
-                if (send(sockfd_client, response.c_str(), response.size(), 0) < 0) {
-                        std::cout << "[" << id << "]: Could not send. errno: " << errno << std::endl;
-                }
-                else {
-                        std::cout << "[" << id << "]: Finished\n\n";
-                }
-
-                close(sockfd_client);
-        }
-}
-
-void signal_handler(int signum)
+void SignalHandler(int signum)
 {
         std::cout << "Receied signal: " << signum << std::endl;
         exit(signum);
@@ -61,7 +23,7 @@ int main()
         int sockfd = socket(AF_INET, SOCK_STREAM, 0);
         if (sockfd < 0) {
                 std::cout << "Cannot create socket.\n";
-                exit(1);
+        	exit(1);
         }
 
         int opt = 1;
@@ -86,22 +48,34 @@ int main()
                 return 1;
         }
 
-        std::signal(SIGINT, signal_handler);
-        std::signal(SIGTERM, signal_handler);
+        std::signal(SIGINT, SignalHandler);
+        std::signal(SIGTERM, SignalHandler);
 
-        int counter = 0;
-        int worker_num = WORKER_NUM;
-        std::vector<pthread_t> workers(worker_num);
-        std::vector<struct arg_struct> args(worker_num);
+        int id = 0;
+	struct sockaddr_storage client_addr;
+	socklen_t client_addr_len = sizeof(client_addr);
+        while(1) {
+		std::cout << "[" << id << "]: accepting...\n";
+		int sockfd_client = accept(sockfd, (struct sockaddr *) &client_addr, &client_addr_len);
 
-        for (int i = 0; i < worker_num; ++i) {
-                args[i].sockfd = sockfd;
-                args[i].id = counter++;
-                pthread_create(&workers[i], NULL, &run, (void *)&args[i]);
-        }
-        for (int i = 0; i < worker_num; ++i) {
-                pthread_join(workers[i], NULL);
-        }
+		/* receive */
+		std::cout << "[" << id << "]: receiving...\n";
+		char buff[BUFF_SIZE] = {0};
+		recv(sockfd_client, buff, BUFF_SIZE, 0);
+		std::cout << "[" << id << "]: message from client: " << buff << std::endl;
+
+		/* send */
+		std::string response(buff);
+		if (send(sockfd_client, response.c_str(), response.size(), 0) < 0) {
+			std::cout << "[" << id << "]: Could not send message. errno: " << errno << std::endl;
+		}
+		else {
+			std::cout << "[" << id << "]: Finished\n\n";
+		}
+
+		close(sockfd_client);
+		++id;
+	}
 
         std::cout << "Shut down server.\n";
         close(sockfd);
